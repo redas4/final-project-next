693faddd75ccd27cec84c1b266163e0001631169
redas4 <ioffredap4@gmail.com>
2023-12-11 11:20:12 -0500

    and update imports

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,7 @@
 import RegisterForm from "@/components/RegisterForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../utils/authOptions";
 
 
 export default async function Register(){
